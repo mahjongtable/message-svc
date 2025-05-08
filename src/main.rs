09438ebec7800fd9baabc7505e1408386351cb4c
illusion <1161524405@qq.com>
2025-05-09 01:40:08 +0800

    The methos of the message serive to be finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use message_svc::pb::{
+    SendEmailRequest, SendEmailResponse, SendSmsRequest, SendSmsResponse, message_server::Message,
+    message_server::MessageServer,
+};
+use tonic::{Request, Response, Status};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let message_svc = MessageService {};
+
+    tonic::transport::Server::builder()
+        .add_service(MessageServer::new(message_svc))
+        .serve("[::1]:50053".parse()?)
+        .await?;
+
+    Ok(())
+}
+
+pub struct MessageService {}
+
+#[tonic::async_trait]
+impl Message for MessageService {
+    async fn send_sms(
+        &self,
+        req: Request<SendSmsRequest>,
+    ) -> Result<Response<SendSmsResponse>, Status> {
+        todo!()
+    }
+
+    async fn send_email(
+        &self,
+        req: Request<SendEmailRequest>,
+    ) -> Result<Response<SendEmailResponse>, Status> {
+        todo!()
+    }
 }
